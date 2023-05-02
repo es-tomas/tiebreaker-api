261d645aa70a2553e27ed722588201c6d416388b
Tomas Escamez <tomase@gmail.com>
2023-05-02 18:44:32 +0200

    Add controller factory handler

--- controllers/tournamentController.js
+++ controllers/tournamentController.js
@@ -1,6 +1,5 @@
 const Tournament = require('../models/tournamentModel');
-const APIFeatures = require('../utils/apiFeatures');
-const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
 
 exports.aliasTopTournaments = (req, res, next) => {
   req.query.limit = '5';
@@ -9,75 +8,13 @@ exports.aliasTopTournaments = (req, res, next) => {
   next();
 };
 
-exports.getAllTournaments = catchAsync(async (req, res) => {
-  // EXECUTE QUERY
-  const features = new APIFeatures(Tournament.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
+exports.getAllTournaments = factory.getAll(Tournament);
 
-  const tournaments = await features.query;
+exports.getTournament = factory.getOne(Tournament);
+// factory.getOne(Tournament, { path: 'reviews' });
 
-  // SEND RESPONSE
-  res.status(200).json({
-    status: 'success',
-    results: tournaments.length,
-    data: {
-      tournaments,
-    },
-  });
-});
+exports.createTournament = factory.createOne(Tournament);
 
-exports.getTournament = catchAsync(async (req, res) => {
-  const tournament = await Tournament.findById(req.params.id);
-  // Tour.findOne({ _id: req.params.id })
+exports.updateTournament = factory.updateOne(Tournament);
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tournament,
-    },
-  });
-});
-
-exports.createTournament = catchAsync(async (req, res) => {
-  // const newTour = new Tour({})
-  // newTour.save()
-
-  const newTournament = await Tournament.create(req.body);
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour: newTournament,
-    },
-  });
-});
-
-exports.updateTournament = catchAsync(async (req, res) => {
-  const tournament = await Tournament.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    {
-      new: true,
-      runValidators: true,
-    }
-  );
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tournament,
-    },
-  });
-});
-
-exports.deleteTournament = catchAsync(async (req, res) => {
-  await Tournament.findByIdAndDelete(req.params.id);
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-});
+exports.deleteTournament = factory.deleteOne(Tournament);
