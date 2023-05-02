d34d662505ec629dc2564a25e3ec54a875f2bcf7
Tomas Escamez <tomase@gmail.com>
2023-05-02 11:10:26 +0200

    Secure API

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -9,17 +13,56 @@ const tournamentRouter = require('./routes/tournamentRoutes');
 
 const app = express();
 
+// 1) GLOBAL MIDDLEWARES
+
+// Set Security HTTP Headers
+app.use(helmet());
+
+// Development Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+}); // Allows 100 requests, from the same IP in 1 hour
+app.use('/api', limiter);
 
-app.use((req, res, next) => {
-  // console.log('Hello from the middleware');
-  next();
-});
+// Body parser, reading data from body into req.body
+app.use(
+  express.json({
+    limit: '10kb', // To preven denial of services if the attacker sends large req body
+  })
+);
 
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter poluttion
+app.use(
+  hpp({
+    whitelist: [
+      // allow to pass these as a parameter more than once in a request
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
